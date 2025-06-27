6fb78d1272fafb9166a95c22c982d842b80b3897
Irsan Arisandy <irsanarisandy@hotmail.com>
2025-06-27 15:38:10 +1000

    Create state avg price API

---
+++ app/utils/parse.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def get_avg_data() -> dict[str, float]:
+    state_count = dict()
+    state_price = dict()
+    with open("data/coding_challenge_prices.csv", "r", encoding="utf-8") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # skip first row, assume it is header
+        for row in csv_reader:
+            state_id = row[0].upper()
+            state_count[state_id] = state_count.get(state_id, 0) + 1
+            state_price[state_id] = round(state_price.get(state_id, 0) + float(row[1]), 2)
+    return { key: round(state_price[key] / state_count[key], 2) for key in state_count.keys() }
